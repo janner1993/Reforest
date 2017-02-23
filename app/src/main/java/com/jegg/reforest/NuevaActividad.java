10a5f959b40318da4ca188fb4c8a6e9f069555b0
ING_JANNER <ing_jannergamarra@hotmail.com>
2017-02-22 19:20:52 -0500

    se creo la clase SharedPreferences

--- app/src/main/java/com/jegg/reforest/NuevaActividad.java
+++ app/src/main/java/com/jegg/reforest/NuevaActividad.java
@@ -1,16 +1,27 @@
 package com.jegg.reforest;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class NuevaActividad extends AppCompatActivity {
+    private EditText cajaNombre;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_actividad);
+        cajaNombre = (EditText) findViewById(R.id.etNombreActividad);
+        spinner = (Spinner) findViewById(R.id.splistaActividades);
     }
 
     @Override
@@ -24,4 +35,23 @@ public class NuevaActividad extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void nuevaActividad (View v){
+        String nombreActividad = cajaNombre.getText().toString();
+        String tipoActividad = spinner.getSelectedItem().toString();
+        Log.e("tipo", tipoActividad);
+
+
+        if (nombreActividad.equals("")){ // el usuario no ha digitado nada
+            Toast.makeText(NuevaActividad.this, "LLENAR EL CAMPO DE NOMBRE", Toast.LENGTH_SHORT).show(); // mostrar un mensaje
+        }
+        else {
+            SharedPreferences prefs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("nombre", nombreActividad);
+            editor.putString("tipoactividad", tipoActividad);
+            editor.apply();
+        }
+    }
+
 }
