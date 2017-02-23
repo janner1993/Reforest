658138533feb1048971fb52ac6401cc1b1c4daa9
OscarV <ing.odvega@gmail.com>
2017-02-22 22:05:50 -0500

    nuevo checkbox

--- app/src/main/java/com/jegg/reforest/SeleccionarActividades.java
+++ app/src/main/java/com/jegg/reforest/SeleccionarActividades.java
@@ -3,15 +3,25 @@ package com.jegg.reforest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v4.widget.CompoundButtonCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class SeleccionarActividades extends AppCompatActivity {
     private String nombredeactividad;
     private String tipoactividad;
     private SharedPreferences mispreferencias;
+    CheckBox nueva;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +30,24 @@ public class SeleccionarActividades extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // habilitar el boton hacia atras
         mispreferencias=getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
         nombredeactividad= mispreferencias.getString("nombre","no existe la variable");
-        tipoactividad =mispreferencias.getString("tipoactividad", "no existe la variable");
+        tipoactividad = mispreferencias.getString("tipoactividad", "no existe la variable");
+        Log.e("shareddata:",nombredeactividad + " "+ tipoactividad);
+        Log.e("tipoactividad:",tipoactividad);
+        LinearLayout selectActividades = (LinearLayout)findViewById(R.id.lyactividades);
+        nueva = new CheckBox(SeleccionarActividades.this);
+        nueva.setText(nombredeactividad);
+        selectActividades.addView(nueva);
+        nueva.setTop(50);
+        nueva.setTextSize(20);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(10,10,20,20);
+        nueva.setLayoutParams(lp);
+        nueva.setTypeface(null, Typeface.BOLD);
+        int states[][] = {{android.R.attr.state_checked}, {}};
+        int colors[] = {Color.BLACK, Color.BLACK};
+        CompoundButtonCompat.setButtonTintList(nueva, new ColorStateList(states, colors));
+
+
 
     }
 
